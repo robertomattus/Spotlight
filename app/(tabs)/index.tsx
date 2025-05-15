c0ab6fae56e5255278684073ff2b046f5d63c07a
robertomattus <robertmatt113@gmail.com>
2025-05-15 01:17:17 -0600

    feature: implement createUser mutation and save the user to db webhook event

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 import { Pressable, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { use } from "react";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
-      <Link href={"/notifications"}>Feed screen in tabs</Link>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: "white" }}>Signout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
